297ea2e876ec3e13276f7826b3fb57cd9c350288
JuniorCode <JuniorCode@users.noreply.github.com>
2018-10-22 11:37:10 -0400

    Update tts.js

--- tts.js
+++ tts.js
@@ -2,11 +2,12 @@ responsiveVoice.setDefaultVoice("US English Male");
 
 if (annyang) {
   var commands = {
-    '*text': function(text) {
+    "*text": function(text) {
       responsiveVoice.speak(text);
     }
   };
+
   annyang.addCommands(commands);
-  
+
   annyang.start();
 }
